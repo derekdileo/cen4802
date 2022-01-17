b673dae6f4ecf86134264f0c5c29e495f6fa04c3
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-01-17 12:06:29 -0500

    Add fibRecursive method and call from main(String[] args) after collecting user-selected position choice using Scanner(System.in).

---
+++ src/com/derekdileo/FibonacciCalculator.java
@@ -0,0 +1,29 @@
+package com.derekdileo;
+
+import java.util.Scanner;
+
+public class FibonacciCalculator {
+
+    public static int fibRecursive(int n) {
+        if(n <= 1) {
+            return n;
+        } else {
+            return fibRecursive(n - 1) + fibRecursive(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Welcome user and request the psotion to be computed
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Fibonacci Calculator");
+        System.out.print("\nPlease enter the position to be computed: ");
+        int position = scanner.nextInt();
+
+        // Compute the nth Fibonacci number
+        int result = fibRecursive(position);
+
+        // Print results using variables
+        System.out.printf("\nThe %dth position of the Fibonacci sequence is %d", position, result);
+
+    }
+}
